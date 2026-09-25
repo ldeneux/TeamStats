62bba318336f2784a09d9439c3a52ea8dac095ff
DENEUX Lionel OBS/OBF <NFTR4813@orange.com>
2026-09-25 22:56:32 +0200

    v1.5 teamstats

--- types/basketball.ts
+++ types/basketball.ts
@@ -1,7 +1,6 @@
 export interface Player { id: string; number: number; name: string; }
 export interface GameEvent { id: string; timestamp: string; quarter: number; clockTime: string; actionType: string; playerId: string; details?: any; }
 export interface GameState {
-  id: string;
   teamHome: string;
   teamAway: string;
   matchDate: string;
