81f1af9b8344523eab6e5e1fe5c63f766ffd43cc
DENEUX Lionel OBS/OBF <NFTR4813@orange.com>
2026-09-25 21:53:05 +0200

    v1.0 teamstats

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'Sathonay Basket Stats' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="fr"><body>{children}</body></html>;
+}
